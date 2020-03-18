9af7a27c8ebb9d5fd2fa52b3148b25e6368ca963
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-03-18 18:32:46 +0000

    Pass first test

---
+++ lib/shop.rb
@@ -0,0 +1,5 @@
+class Shop
+  def self.checkout(items)
+    return 50
+  end
+end
---
+++ spec/shop_spec.rb
@@ -0,0 +1,9 @@
+require 'shop'
+
+describe Shop do
+  describe '#checkout' do 
+    it 'takes an input of A and returns 50' do
+      expect(Shop.checkout('A')).to eq(50)
+    end
+  end
+end
